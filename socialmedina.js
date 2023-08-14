a53bc0dc6ee39e59e598fbd51c47d2dfd18f378e
B V CHARAN KUMAR <120728597+bvcharan26@users.noreply.github.com>
2023-08-14 15:50:59 +0530

    added socialmedia.js

---
+++ socialmedina.js
@@ -0,0 +1,9 @@
+function github(){
+    window.location.href='https://github.com/econtentrgukt';
+}
+function mail(){
+    window.location.href='mailto:econtentrgukt@gmail.com';
+}
+function linkedln(){
+    window.location.href='https://www.linkedin.com/in/charan-kumar-b-v-20917425b/';
+}
