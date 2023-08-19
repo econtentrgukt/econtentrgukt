ee976d2629a10766393ec47b1805c8fbe181438f
B V CHARAN KUMAR <120728597+bvcharan26@users.noreply.github.com>
2023-08-19 10:55:47 +0530

    Added gatecs.html gatecs.js gatecs.pdf

---
+++ gatecs.js
@@ -0,0 +1,120 @@
+function maths(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function dld(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function coa(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function pds(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function algo(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function tc(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function cd(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function os(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function dbms(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
+function cn(){
+    var name=prompt("Enter 1 for materials\n2 for Reference videos");
+    if(name==1){
+        window.location.href='#';
+    }
+    else if(name==2){
+        window.location.href='#';
+    }
+    else{
+        alert("Invalid Input");
+    }
+}
