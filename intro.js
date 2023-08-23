389cd37db88dfd850591e35f7a69becb522d305c
B V CHARAN KUMAR <120728597+bvcharan26@users.noreply.github.com>
2023-08-23 22:14:20 +0530

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,1 @@
+alert("Welcome\nWe are further planning to make a Student Teacher Interactive platform,So if you know Backend Development your contribution is valuable.\nFor further:econtentrgukt@gmail.com")
